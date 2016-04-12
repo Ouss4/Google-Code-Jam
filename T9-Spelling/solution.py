358bb794f580aafe60156b7731f947cec0b96f50
Ouss4 <guettouche_4@hotmail.fr>
2016-04-12 17:51:10 +0200

    Added solution

---
+++ T9-Spelling/solution.py
@@ -0,0 +1,35 @@
+def toT9(text):
+
+    translator = {2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno',
+                  7:'pqrs', 8:'tuv', 9:'wxyz'}
+
+    output = ""
+    oldKey = -1
+
+    for c in text:
+        if c == " ":
+            if oldKey == 0:
+                output += " "
+            output += "0"
+            oldKey = 0
+        else:
+            for key, val in translator.items():
+                i = val.find(c) + 1
+                if i > 0:
+                    if oldKey == key:
+                        output += " "
+                    output += str(key) * i
+                    oldKey = key
+    return output
+
+
+fileName = "C-large-practice.in"
+with open(fileName, "r") as inputFile:
+    i = 1
+    # Read the file and remove the first element and the last \n
+    fileContent = inputFile.read().split("\n")[1:-1] 
+
+    for text in fileContent:
+        t9Text = toT9(text)
+        print("Case #{}: {}".format(i, t9Text))
+        i += 1
